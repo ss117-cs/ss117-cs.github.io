57b0a798d79cfdc81e92e107bc7e87c0d8ada7b7
c3subramanianshankar <32652945+c3subramanianshankar@users.noreply.github.com>
2020-08-02 21:07:07 -0700

    Final

--- js/map.js
+++ js/map.js
@@ -3,11 +3,11 @@
   var countriesJson=d3.json("data/raw_data/countries.geo.json")
 
   var width =  600;
-  var height = 300;
+  var height = 400;
 
   var margin = {
     top: 10,
-    bottom: 100,
+    bottom: 50,
     left: 130,
     right: 10
   }
